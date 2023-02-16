2e262f590b52897c2579d444bbf22ef856ba4607
Smith <111470120+Smith2067@users.noreply.github.com>
2023-02-15 17:33:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var newDiv = document.createElement('div');
+newDiv.textContent = "This is the DIV.";
+document.body.appendChild(newDiv);
+
+var newParagraph = document.createElement('p');
+newParagraph.textContent = "This is the paragraph.";
+document.body.appendChild(newParagraph);
